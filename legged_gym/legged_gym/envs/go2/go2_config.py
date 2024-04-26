046c3eacec7c1205d789bf17a45a8989876ab738
hanlinniu <niuhanlin@icloud.com>
2024-04-27 00:29:07 +0100

    update

--- legged_gym/legged_gym/envs/go2/go2_config.py
+++ legged_gym/legged_gym/envs/go2/go2_config.py
@@ -32,7 +32,7 @@ from legged_gym.envs.base.legged_robot_config import LeggedRobotCfg, LeggedRobot
 
 class Go2RoughCfg( LeggedRobotCfg ):
     class init_state( LeggedRobotCfg.init_state ):
-        pos = [0.0, 0.0, 0.42] # x,y,z [m]
+        pos = [0.0, 0.0, 0.35] # x,y,z [m]
         default_joint_angles = { # = target angles [rad] when action = 0.0
             'FL_hip_joint': 0.1,   # [rad]
             'RL_hip_joint': 0.1,   # [rad]
