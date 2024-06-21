fe9103dc02a7654fc6b801ea86e9316d7310452a
Niu, Hanlin <hanlin.niu@ukaea.uk>
2024-06-21 17:36:31 +0100

    update

--- rsl_rl/rsl_rl/modules/depth_backbone.py
+++ rsl_rl/rsl_rl/modules/depth_backbone.py
@@ -114,10 +114,5 @@ class DepthOnlyFCBackbone58x87(nn.Module):
 
     def forward(self, images: torch.Tensor):
         images_compressed = self.image_compression(images.unsqueeze(1))    # images size is [192, 58, 87].   images.unsqueeze(1) size is [192, 1, 58, 87].  
-        latent = self.output_activation(images_compressed)                 # images_compressed size is []
-        print("images size is: ", images.size())
-        print("images.unsqueeze(1) size is: ", images.unsqueeze(1).size())
-        print("images_compressed size is: ", images_compressed.size())
-        print("latent size is: ", latent.size())
-
+        latent = self.output_activation(images_compressed)                 # images_compressed size is [192, 32].   latent size is [192, 32]  
         return latent
