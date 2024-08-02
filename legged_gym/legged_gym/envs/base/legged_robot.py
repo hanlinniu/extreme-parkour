35ca3eac3ba6a2e970b7e93e1dbc4d7dd683a139
hanlinniu <niuhanlin@icloud.com>
2024-08-02 03:27:52 +0100

    update

--- legged_gym/legged_gym/envs/base/legged_robot.py
+++ legged_gym/legged_gym/envs/base/legged_robot.py
@@ -756,9 +756,9 @@ class LeggedRobot(BaseTask):
             env_ids (List[int]): ids of environments being reset
         """
         # Implement Terrain curriculum
-        print("test 1 !!!!!!!!!!")
+        # print("test 1 !!!!!!!!!!")
         if not self.init_done:
-            print("test 2 !!!!!!!!!!")
+            # print("test 2 !!!!!!!!!!")
             # don't change on initial reset
             return                       # in the training beginning, this one is not executed
         
@@ -780,7 +780,7 @@ class LeggedRobot(BaseTask):
         self.env_goals[:] = torch.cat((temp, last_col.repeat(1, self.cfg.env.num_future_goal_obs, 1)), dim=1)[:]
         self.cur_goals = self._gather_cur_goals()
         self.next_goals = self._gather_cur_goals(future=1)
-        print("test 3 !!!!!!!!!!")
+        # print("test 3 !!!!!!!!!!")
 
     #----------------------------------------
     def _init_buffers(self):
